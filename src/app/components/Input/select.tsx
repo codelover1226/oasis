5d9a0f94319abeecdb0287f3efdd1d9f44418536
Jay <wmeng0103@gmail.com>
2024-06-02 09:00:14 +0900

    header fixed

--- src/app/components/Input/select.tsx
+++ src/app/components/Input/select.tsx
@@ -19,16 +19,16 @@ const Select: React.FC<SelectProps> = ({ options, onChange, id, name }) => {
       {/* <p className="text-xs mb-1 pl-3">
         {name}
       </p> */}
-    <select
-      className="bg-main-bg border-[2px] w-[calc(100vw-24px)] rounded-full sm:w-full mx-3"
-      style={{ padding: '8px 12px' }}
-      onChange={onChange}
-      id={id}
-    >
-      {options.map((option, index) => (
-        <option key={index} value={option.value}>{option.label}</option>
-      ))}
-    </select>
+      <select
+        className="bg-main-bg border-[2px] w-[calc(100vw-24px)] rounded-full sm:w-full mx-3 h-[43px]"
+        style={{ padding: '8px 12px' }}
+        onChange={onChange}
+        id={id}
+      >
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>{option.label}</option>
+        ))}
+      </select>
     </div>
   );
 };
